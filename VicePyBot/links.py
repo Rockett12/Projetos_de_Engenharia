44cb7d61a2428d9b7c687c23fe47e79cea1ea1ac
Diego Dantas Figueiredo <35031027+Feelss@users.noreply.github.com>
2018-07-11 21:56:19 -0300

    Updates

--- VicePyBot/links.py
+++ VicePyBot/links.py
@@ -1,22 +1,37 @@
 links = []
+yea = False
 
 @bot.message_handler(commands=['links'])
 def link(message):
-    ans = str(len(links)) + 'links\n'
+    ans = str(len(links)) + ' links\n'
     for i in range(len(links)):
-        if isinstance(links[i], str):
-            ans += str(i) + '- ' + links[i] + '\n'
-        else:
+        if isinstance(links[i], list):
             ans += str(i) + '- ' + links[i][0] + '\n'
+        else:
+            ans += str(i) + '- ' + links[i] + '\n'
     bot.send_message(message.chat.id, ans)
 
 @bot.message_handler(commands=['links_add'])
 def addition(message):
-    if message.Document != None:
-        links.append([message.Document.file_name + ' - Disponivel para download, acione o comando com indice para baixar.', message.Document.file_id])
-    else:
-        msg = message.text.replace('/links_add', '')
-        links.append(msg.text)
+    try:
+        ans = 'Adicionado com sucesso!'
+        msg = message.text.replace('\links_add', '')
+        if msg == '':
+            ans = 'Aguardando arquivo...'
+            yea = True
+        else:
+            links.append(msg)
+    except Exception as e:
+        ans = 'Erro, tente novamente'
+    bot.send_message(message.chat.id, ans)
+
+
+@bot.message_handler(content_types= 'document')
+def arquivo(message):
+    if yea:
+        links.append([str(message.Document.file_name) + ' Disponivel para download, use o comando com o devido indice para baixar.', message.Document.file_id])
+        bot.send_message(message.chat.id, 'Adicionado com sucesso!')
+        yea = False
 
 @bot.message_handler(commands=['links_download'])
 def download(message):
